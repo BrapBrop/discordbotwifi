cdcf983e37af5fc15714c7de00be4577f51b1adb
BrapBrop <51305823+BrapBrop@users.noreply.github.com>
2019-06-02 19:31:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'MzY2NDMwMTM4Mjk0MDc1NDEy.XPRr2w.wcoMRKrpSg3E3bCO-WwF3dGEdf0';
+
+const PREFIX = '!'
+
+var verison = '1.0.1';
+
+bot.on('ready', () =>{
+   console.log('This bot is online!');
+});
+
+bot.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ")
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong!')
+            break;
+        case 'server':
+            message.channel.send('https://discord.gg/uX6vkUr')
+            break;
+        case 'info':
+            if(args[1] === 'version'){
+                message.channel.send('version ' + verison);
+            }else{
+                message.channel.send('Invalid Args')
+            }
+        case 'clear':
+            if(!args[1]) return message.reply('Error please define second arg')
+            message.channel.bulkDelete(args[1]);
+            break;
+        case 'embed':
+            if(args[3]= 'help'){
+                const embed = new Discord.RichEmbed()
+                .addField('Player Name', message.author.username);
+                message.channel.sendEmbed(embed);
+                break;
+            }
+    }
+})
+
+bot.login(token);
+
+
+
